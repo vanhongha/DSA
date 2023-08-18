2c65b27be846eec0780356e714d7a2c15159cf14
vanhongha <honghaa8@gmail.com>
2023-08-18 15:20:34 +0900

    Empty main

--- DSA/DSA.cpp
+++ DSA/DSA.cpp
@@ -4,27 +4,5 @@
 
 int main()
 {
-	Queue *queue = new Queue();
-	queue->Enqueue(1);
-	queue->Enqueue(2);
-	queue->Enqueue(3);
-	queue->Print();
 
-	cout << "Dequeue: " << queue->Dequeue() << endl;
-	queue->Print();
-
-	cout << "Get front: " << queue->GetFront() << endl;
-	queue->Print();
-
-	Stack* stack = new Stack();
-	stack->Push(1);
-	stack->Push(2);
-	stack->Push(3);
-	stack->Print();
-
-	cout << "Pop: " << stack->Pop() << endl;
-	stack->Print();
-
-	cout << "Get top: " << stack->GetTop() << endl;
-	stack->Print();
 }
